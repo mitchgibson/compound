eebc875262e86f019c7afdc481b669f16c4a9c2d
knakmitch <mitch.delachevrotitere@knak.com>
2024-06-11 21:57:50 -0400

    Add VueUse and VuePress

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <Toolbar>
-    <template #start></template>
+    <template #start>Battery: {{ levelPercent }}</template>
     <template #center></template>
     <template #end>
       <Button outlined :icon="`pi pi-${themeIcon}`" @click="theme.toggleTheme" class="mr-2" />
@@ -13,7 +13,10 @@ import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
 import { useThemeStore } from "../../stores/ThemeStore";
 import { computed } from "vue";
+import { useBattery } from '@vueuse/core'
 
+const { level } = useBattery();
 const theme = useThemeStore();
 const themeIcon = computed(() => theme.isDark ? 'moon' : 'sun');
+const levelPercent = computed(() => Math.round(level.value * 100) + '%');
 </script>
---
+++ src/stores/ThemeStore.md
@@ -0,0 +1,5 @@
+---
+outline: deep
+---
+
+# Theme Store
